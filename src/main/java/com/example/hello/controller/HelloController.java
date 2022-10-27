b2db301a168b51f24fdc9f260939d89949b1152c
yjyj1023 <lyj19961023@gmail.com>
2022-10-27 11:26:11 +0900

    Query parameter 추가

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -1,13 +1,33 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/v1/get-api")
 public class HelloController {
 
-    @RequestMapping("/hello")
+    @GetMapping(value = "/hello")
     public String hello(){
         return "Hello World";
+    }
+
+    @GetMapping(value = "/name")
+    public String getName(){
+        return "YeonJae";
+    }
+
+    //http://localhost:8080/api/v1/get-api/variable1/1
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    //http://localhost:8080/api/v1/get-api/request1?name=YeonJae&email=70003738@gmail.com&organization=멋사
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam String organization){
+        return name+" "+email+" "+organization;
     }
 }
