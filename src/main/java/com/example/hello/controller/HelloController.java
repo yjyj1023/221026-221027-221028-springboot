c47093dca679c7219cc0c17e201fe2354af422bf
yjyj1023 <lyj19961023@gmail.com>
2022-10-27 11:49:16 +0900

    Map으로 RequestParam받기 추가

--- src/main/java/com/example/hello/controller/HelloController.java
+++ src/main/java/com/example/hello/controller/HelloController.java
@@ -2,23 +2,25 @@ package com.example.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class HelloController {
 
     @GetMapping(value = "/hello")
-    public String hello(){
+    public String hello() {
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
-    public String getName(){
+    public String getName() {
         return "YeonJae";
     }
 
     //http://localhost:8080/api/v1/get-api/variable1/1
     @GetMapping(value = "/variable1/{variable}")
-    public String getVariable1(@PathVariable String variable){
+    public String getVariable1(@PathVariable String variable) {
         return variable;
     }
 
@@ -27,7 +29,17 @@ public class HelloController {
     public String getRequestParam1(
             @RequestParam String name,
             @RequestParam String email,
-            @RequestParam String organization){
-        return name+" "+email+" "+organization;
+            @RequestParam String organization) {
+        return name + " " + email + " " + organization;
+    }
+
+    //http://localhost:8080/api/v1/get-api/request1?name=YeonJae&email=70003738@gmail.com&organization=멋사
+    @GetMapping(value = "/request2")
+    public String getVariable2(@RequestParam Map<String, String> param) {
+        param.entrySet().forEach((map) -> {
+            System.out.printf("key:%s value:%s\n", map.getKey(), map.getValue());
+        });
+        return "request2가 호출 완료 되었습니다";
+
     }
 }
