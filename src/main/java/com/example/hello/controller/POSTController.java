d63bd33e69c2799dd3bc92c7e59ddb540e1bd780
yjyj1023 <lyj19961023@gmail.com>
2022-10-27 14:20:27 +0900

    @RequestBody를 활용한 POST메서드 구현

--- src/main/java/com/example/hello/controller/POSTController.java
+++ src/main/java/com/example/hello/controller/POSTController.java
@@ -1,9 +1,9 @@
 package com.example.hello.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hello.domain.dto.MemberDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/post-api")
@@ -12,5 +12,21 @@ public class POSTController {
     @RequestMapping(value = "/domain", method = RequestMethod.POST)
     public String postExample() {
         return "Hello Post API";
+    }
+
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData){
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(map -> {
+            sb.append(map.getKey()+" : " + map.getValue()+"\n");
+        });
+
+        return sb.toString();
+    }
+
+    @PostMapping("/member2")
+    public String postMember2(@RequestBody MemberDto memberDto){
+        return memberDto.toString();
     }
 }
