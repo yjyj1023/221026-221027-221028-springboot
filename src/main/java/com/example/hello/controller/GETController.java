abb2fd05558cc7d7e4623a648b5373c2340cef2c
yjyj1023 <lyj19961023@gmail.com>
2022-10-28 11:32:55 +0900

    log남기기

--- src/main/java/com/example/hello/controller/GETController.java
+++ src/main/java/com/example/hello/controller/GETController.java
@@ -1,27 +1,32 @@
 package com.example.hello.controller;
 
 import com.example.hello.domain.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GETController {
 
     @GetMapping(value = "/hello")
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName() {
+        log.info("getName로 요청이 들어왔습니다.");
         return "YeonJae";
     }
 
     //http://localhost:8080/api/v1/get-api/variable1/1
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable) {
+        log.info("getVariable1로 요청이 들어왔습니다. variable: {}",variable);
         return variable;
     }
 
